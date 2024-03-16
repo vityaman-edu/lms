b40f7f2905f0e341f5f93c3fd47471d1272ccb87
Victor Smirnov <vityaman.dev@yandex.ru>
2024-03-16 14:22:21 +0300

    #4 [setup] Created an empty Spring Boot project

---
+++ gateway/src/main/kotlin/ru/itmo/lms/gateway/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package ru.itmo.lms.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayApplication>(*args)
+}
