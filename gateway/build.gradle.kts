ee700d46eaf1f92bb111b7e376a1e6825c65afa1
Victor Smirnov <vityaman.dev@yandex.ru>
2024-03-16 15:18:43 +0300

    #4 [setup] Added OpenAPI Rest Controller generation from spec

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -1,48 +1,95 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-	id("org.springframework.boot") version "3.2.3"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.openapi.generator") version "5.3.0"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "ru.itmo"
 version = "0.0.1"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "21"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+fun generateOpenApiSpec(taskName: String, spec: String, pkg: String) =
+        tasks.register<GenerateTask>(taskName) {
+            group = "Open API Source Code Generation"
+            description = "Generates kotlin classes from an Open API specification"
+            verbose = false
+            generatorName = "kotlin-spring"
+            inputSpec = spec
+            outputDir = "${layout.projectDirectory}/build/generated"
+            packageName = pkg
+            modelPackage = pkg
+            generateModelTests = false
+            generateApiTests = false
+            configOptions = mapOf(
+                    "serializationLibrary" to "jackson",
+                    "enumPropertyNaming" to "UPPERCASE",
+                    "dateLibrary" to "java8",
+                    "bigDecimalAsString" to "true",
+                    "hideGenerationTimestamp" to "true",
+                    "useBeanValidation" to "false",
+                    "performBeanValidation" to "false",
+                    "openApiNullable" to "false",
+                    "reactive" to "true",
+                    "interfaceOnly" to "true",
+            )
+        }
+
+generateOpenApiSpec(
+        taskName = "generateApi",
+        spec = "${layout.projectDirectory}/src/main/resources/static/openapi/api.yml",
+        pkg = "ru.itmo.lms.gateway.api.http",
+)
+
+
+tasks.compileKotlin.configure {
+    dependsOn(tasks.getByName("generateApi"))
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("${layout.projectDirectory}/build/generated/src/main/kotlin")
+        }
+    }
 }
--- gateway/src/main/kotlin/ru/itmo/lms/gateway/api/http/MonitoringHttpApi.kt
+++ gateway/src/main/kotlin/ru/itmo/lms/gateway/api/http/MonitoringHttpApi.kt
@@ -2,14 +2,12 @@ package ru.itmo.lms.gateway.api.http
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import ru.itmo.lms.gateway.api.http.apis.MonitoringApi
 
 @RestController
-@RequestMapping("/monitoring")
-class MonitoringHttpApi {
-    @GetMapping("/ping")
-    suspend fun ping(): ResponseEntity<String> {
+class MonitoringHttpApi : MonitoringApi {
+    override suspend fun monitoringPingGet(): ResponseEntity<String> {
         return ResponseEntity.ok("pong")
     }
 }
