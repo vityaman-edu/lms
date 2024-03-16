987b9b8669e9cf5ee63c95c4a7c5bf5c9dff48eb
Victor Smirnov <vityaman.dev@yandex.ru>
2024-03-16 15:59:45 +0300

    #4 [setup] Added Swagger UI

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -28,6 +28,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.4.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
