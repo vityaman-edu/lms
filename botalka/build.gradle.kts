ce5cd80434663e72b41acbe883630dddc7591c11
vityaman <vityaman.dev@yandex.ru>
2024-03-28 16:50:49 +0300

    #21 Added Homework POST endpoint

--- botalka/build.gradle.kts
+++ botalka/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
 
-    id("org.openapi.generator") version "5.3.0"
+    id("org.openapi.generator") version "7.4.0"
 
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.5"
@@ -55,6 +55,7 @@ dependencies {
 
     implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.4.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
 
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     runtimeOnly("org.postgresql:r2dbc-postgresql")
@@ -71,6 +72,8 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("io.projectreactor:reactor-test")
@@ -106,17 +109,14 @@ tasks.register<OpenAPIGenerateTask>(generateControllers) {
     packageName = pkg
     modelPackage = pkg
     modelNameSuffix = "Message"
-    generateModelTests = false
+    generateModelTests = true
     generateApiTests = false
     configOptions =
         mapOf(
+            "useSpringBoot3" to "true",
             "serializationLibrary" to "jackson",
+            "dateLibrary" to "kotlinx-datetime",
             "enumPropertyNaming" to "UPPERCASE",
-            "dateLibrary" to "java8",
-            "bigDecimalAsString" to "true",
-            "hideGenerationTimestamp" to "true",
-            "useBeanValidation" to "false",
-            "performBeanValidation" to "false",
             "openApiNullable" to "false",
             "reactive" to "true",
             "interfaceOnly" to "true",
